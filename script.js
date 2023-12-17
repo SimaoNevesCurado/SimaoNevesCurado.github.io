f90f871d13de9cb480bc9e84c544e97e7fa2b70f
SimaoNevesCurado <simao.curadoo@gmail.com>
2023-12-17 02:12:44 +0000

    Added socials onto the dropdown menu, added animations onto header content

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
 const navigation = document.querySelector(".navigation");
 
+
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navigation.classList.toggle("active");
