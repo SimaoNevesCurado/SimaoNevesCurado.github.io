f9227d10074e6e81db3aaf745c25685488d44634
SimaoNevesCurado <simao.curadoo@gmail.com>
2023-12-29 16:00:09 +0000

    Added JavaScript

---
+++ main.js
@@ -0,0 +1,43 @@
+const hamburger =document.querySelector('.hamburger');
+const navList =document.querySelector('nav ul');
+
+hamburger.addEventListener('click', () =>{
+
+    hamburger.classList.toggle('active');
+    navList.classList.toggle('active');
+});
+
+//dark - light modes
+
+    const darkModeButton = document.getElementById('darkModeButton');
+    const body= document.body;
+    const enableDarkMode= ( )=> {
+        body.classList.add('dark-mode');
+
+    }
+
+    const disableDarkMode= ()=> {
+        body.classList.remove('dark-mode');
+
+    }
+    darkModeButton.addEventListener('change',()=>{
+
+        if(darkModeButton.checked){
+            disableDarkMode()
+        }else{
+            enableDarkMode();
+        }
+
+    });
+
+    //multiple text
+
+    const typed = new Typed('.multiple',{
+
+        strings:['Junior Developer','College Student'],
+        typeSpeed: 100,
+        backSpeed:100,
+        backDelay:1000,
+        loop:true
+
+    });
